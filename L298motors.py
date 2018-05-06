2c67c33d6b597dac9182881ca91f24765e7d427d
Seth <silver1row@cox.net>
2018-05-06 16:37:06 -0500

    RE: Adding more software
    Hello...this software may be different but it does work for now.
    
    Seth

---
+++ L298motors.py
@@ -0,0 +1,38 @@
+import Adafruit_BBIO.GPIO as GPIO
+import time
+from time import sleep
+
+EnA = "P9_21"
+ln1 = "P9_22"
+ln2 = "P9_12"
+
+print "Initialize"
+
+GPIO.setup("P9_21", GPIO.OUT)
+GPIO.setup("P9_22", GPIO.OUT)
+GPIO.setup("P9_12", GPIO.OUT)
+
+GPIO.output("P9_21", GPIO.LOW)
+GPIO.output("P9_22", GPIO.LOW)
+GPIO.output("P9_12", GPIO.LOW)
+
+while True:
+
+    for i in range(10):
+        EnA = GPIO.output("P9_21", GPIO.HIGH)
+    for i in range(3):
+        ln1 = GPIO.output("P9_22", GPIO.HIGH)
+    for i in range(5):
+        ln2 = GPIO.output("P9_12", GPIO.HIGH)
+    for i in range(4):
+        ln1 = GPIO.output("P9_22", GPIO.LOW)
+    for i in range(10):
+        EnA = GPIO.output("P9_21", GPIO.LOW)
+        print "Go, Go Bananas!"
+
+        try:
+		
+		except KeyboardInterrupt:
+		    print "I got it over w/!"
+			GPIO.cleanup()
+			quit(sleep)
