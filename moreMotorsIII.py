7a808556ffa4a76123ada02759b316b213ced4aa
Seth <silver1row@cox.net>
2018-05-18 22:55:05 -0500

    updates...
    Just some software to provide people w/ working knowledge of this motor driver...
    
    Enjoy...

--- moreMotorsIII.py
+++ moreMotorsIII.py
@@ -1,63 +1,69 @@
 import Adafruit_BBIO.GPIO as GPIO
 from time import sleep
 
-EnA = "P9_21"
-ln1 = "P9_22"
-ln2 = "P9_12"
+EnA = "P8_13"
+EnB = "P8_16"
+ln1 = "P8_14"
+ln2 = "P8_15"
+ln3 = "P8_17"
+ln4 = "P8_18"
 
 print "Initialize"
 
-GPIO.setup("P9_21", GPIO.OUT)
-GPIO.setup("P9_22", GPIO.OUT)
-GPIO.setup("P9_12", GPIO.OUT)
+GPIO.setup(EnA, GPIO.OUT)
+GPIO.setup(EnB, GPIO.OUT)
+GPIO.setup(ln1, GPIO.OUT)
+GPIO.setup(ln2, GPIO.OUT)
+GPIO.setup(ln3, GPIO.OUT)
+GPIO.setup(ln4, GPIO.OUT)
 
-if EnA != ln1:
-    
-    EnA = GPIO.output("P9_21", GPIO.LOW)
-    ln1 = GPIO.output("P9_22", GPIO.HIGH)
-    ln2 = GPIO.output("P9_12", GPIO.LOW)
+while True:
+
+    GPIO.output(EnA, GPIO.LOW)
+    GPIO.output(ln1, GPIO.LOW)
+    GPIO.output(ln2, GPIO.LOW)
+    GPIO.output(EnB, GPIO.LOW)
+    GPIO.output(ln3, GPIO.LOW)
+    GPIO.output(ln4, GPIO.LOW)
     sleep(8)
+    print "I like bacon!"
 
-    EnA = GPIO.output("P9_21", GPIO.HIGH)
-    ln1 = GPIO.output("P9_22", GPIO.LOW)
-    ln2 = GPIO.output("P9_12", GPIO.HIGH)
+    GPIO.output(EnA, GPIO.HIGH)
+    GPIO.output(EnB, GPIO.HIGH)
+
+    GPIO.output(ln1, GPIO.LOW)
+    GPIO.output(ln2, GPIO.HIGH)
+
+    GPIO.output(ln3, GPIO.LOW)
+    GPIO.output(ln4, GPIO.HIGH)
     sleep(8)
+    print "I am not a cohort!"
 
-    EnA = GPIO.output("P9_21", GPIO.LOW)
-    ln1 = GPIO.output("P9_22", GPIO.LOW)
-    ln2 = GPIO.output("P9_12", GPIO.LOW)
+    GPIO.output(EnA, GPIO.HIGH)
+    GPIO.output(EnB, GPIO.HIGH)
+
+    GPIO.output(ln1, GPIO.LOW)
+    GPIO.output(ln2, GPIO.LOW)
+
+    GPIO.output(ln3, GPIO.LOW)
+    GPIO.output(ln4, GPIO.LOW)
+    sleep(8)
+    print "Heh?"
+	
+	GPIO.output(EnA, GPIO.HIGH)
+    GPIO.output(EnB, GPIO.HIGH)
+
+    GPIO.output(ln1, GPIO.HIGH)
+    GPIO.output(ln2, GPIO.LOW)
+
+    GPIO.output(ln3, GPIO.HIGH)
+    GPIO.output(ln4, GPIO.LOW)
     sleep(8)
     print "I am here now!"
 
-    EnA = GPIO.output("P9_21", GPIO.HIGH)
-    ln1 = GPIO.output("P9_22", GPIO.HIGH)
-    ln2 = GPIO.output("P9_12", GPIO.LOW)
-    sleep(8)
-    print "Flunk-a-Dunk!"
-    
-else:
+    GPIO.output(EnA, GPIO.LOW)
+    GPIO.output(EnB, GPIO.LOW)
+    GPIO.cleanup()
+    quit()
 
-    for i in range(1, 9):
-        EnA = GPIO.output("P9_21", GPIO.HIGH)
-        ln1 = GPIO.output("P9_22", GPIO.LOW)
-        ln2 = GPIO.output("P9_12", GPIO.HIGH)
 
-        EnA = GPIO.output("P9_21", GPIO.HIGH)
-        ln1 = GPIO.output("P9_22", GPIO.HIGH)
-        ln2 = GPIO.output("P9_12", GPIO.LOW)
-        sleep(0.2)
-
-        EnA = GPIO.output("P9_21", GPIO.LOW)
-        ln1 = GPIO.output("P9_22", GPIO.HIGH)
-        ln2 = GPIO.output("P9_12", GPIO.HIGH)
-        sleep(0.2)
-        print "I am still almost there!"
-
-try:
-    EnA = GPIO.output("P9_21", GPIO.OUT)
-    print "Going to sleep now!"
-
-except(KeyboardInterrupt):
-        print "I got it over w/!"
-        GPIO.cleanup()
-        quit()
