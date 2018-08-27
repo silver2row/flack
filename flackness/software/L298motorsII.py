b82748a001d98373ac125977859f5fff187e53d6
silver2row <silver1row@cox.net>
2018-08-27 23:45:39 +0000

    trying w/ this software II

--- flackness/software/L298motorsII.py
+++ flackness/software/L298motorsII.py
@@ -1,32 +1,36 @@
 import Adafruit_BBIO.GPIO as GPIO
 from time import sleep
- 
-EnA = "P9_21"
-In1 = "P9_22"
-In2 = "P9_12"
- 
-for pin in EnA, In1, In2:
-    GPIO.setup(pin, GPIO.OUT)
- 
+import time
+
+EnA = "P9_13"
+In1 = "P9_14"
+In2 = "P9_15"
+
+for fill in EnA, In1, In2:
+    GPIO.setup(fill, GPIO.OUT)
+
 def disable(): GPIO.output(EnA, GPIO.LOW)
 def enable(): GPIO.output(EnA, GPIO.HIGH)
- 
+
 def drive(level1, level2):
     GPIO.output(In1, level1)
-    GPIO.output(In2, level2)
+    GPIO.output(In2,level2)
     enable()
- 
-def stop():    drive(GPIO.LOW,  GPIO.LOW)
-def forward(): drive(GPIO.HIGH, GPIO.LOW)
-def reverse(): drive(GPIO.LOW,  GPIO.HIGH)
- 
-forward(5)
-sleep(2)
-stop(6)
-sleep(2)
-reverse(5)
-sleep(2)
- 
+
+def stop():
+    drive(GPIO.LOW,  GPIO.LOW)
+    time.sleep(2)
+def forward():
+    drive(GPIO.HIGH, GPIO.LOW)
+    time.sleep(9)
+def reverse():
+    drive(GPIO.LOW,  GPIO.HIGH)
+    time.sleep(18)
+
 stop()
-sleep(1)
+forward()
+reverse()
+enable()
 disable()
+drive(1, 2)
+sleep(7)
